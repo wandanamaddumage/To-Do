fe372a96355d36f7d78a023a68fff2b283b51e1a
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-01-14 11:56:55 +0530

    modify: App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import "./styles.css"
+
 export default function App() {
-  return "Hii"
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ui className="list">
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ui>
+    </>
+  )
 }
