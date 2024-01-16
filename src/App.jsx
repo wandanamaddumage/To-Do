0f41d9f1c2e9441a3b420ad7e65d314c8de542bb
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-01-16 22:02:06 +0530

    update: App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos([...todos,{}])
+  }
+  newItem
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item"/>
+          <input 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)} 
+            type="text" 
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
