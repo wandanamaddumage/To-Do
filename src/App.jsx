f086930a65f9a2b6fedac29a3985d0bc037390ab
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-01-22 11:25:05 +0530

    modify: App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,23 @@ export default function App() {
   function handleSubmit(e){
     e.preventDefault()
 
-    setTodos((currentTodo) => {
-      return [...currentTodo,{id: crypto.randomUUID(), title: newItem, completed: false},]
+    setTodos((currentTodos) => {
+      return [...currentTodos,{id: crypto.randomUUID(), title: newItem, completed: false},]
     })
     
-    setTodos((currentTodo) => {
-      return [...currentTodo,{id: crypto.randomUUID(), title: newItem, completed: false},]
-    })
+    setNewItem(" ")
   }
   
-console.log(todos)
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return{...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
 
   return (
     <>
@@ -35,20 +42,16 @@ console.log(todos)
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
+        {todos.map(todo => {
+          return  <li key={todo.id}>
           <label>
-            <input type="checkbox"/>
-            Item 1
+            <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
           </label>
           <button className="btn btn-danger">Delete</button>
         </li>
-        <li>
-          <label>
-            <input type="checkbox"/>
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        })}
       </ul>
     </>
   )
